1df59b95b3c646ca5f35801696346ea0b6f0dfc3
Alberto Magno <am.magno2@gmail.com>
2026-03-11 13:20:21 -0300

    add crud

---
+++ src/main.cpp
@@ -0,0 +1,70 @@
+#include <iostream>
+#include <string>
+
+#include "dao/StudentDAO.cpp"
+#include "models/Student.h"
+
+using namespace std;
+
+void menu() {
+    cout << "\n--- SISTEMA DE MATRÍCULA ---" << endl;
+    cout << "1. Cadastrar Estudante" << endl;
+    cout << "2. Listar Todos" << endl;
+    cout << "3. Buscar por ID" << endl;
+    cout << "4. Atualizar Estudante" << endl;
+    cout << "5. Excluir Estudante" << endl;
+    cout << "0. Sair" << endl;
+    cout << "Escolha: ";
+}
+
+int main() {
+    StudentDAO dao;
+    int option, id;
+
+    do {
+        menu();
+        cin >> option;
+        cin.ignore();
+
+        switch (option) {
+            case 1: { // Cadastrar Estudante
+                Student s;
+                cout << "Nome: ";
+                cin.getline(s.name, 50);
+                cout << "Data Nascimento (DDMMYYYY): ";
+                cin >> s.birth_date;
+                
+                if (dao.create(s)) cout << "Sucesso! ID do novo estudante: " << s._id << endl;
+                break;
+            }
+            case 2: { // Listar Todos
+                dao.listAll();
+                break;
+            }
+            case 3: { // Buscar por ID
+                cout << "Digite o ID: ";
+                cin >> id;
+                Student s = dao.read(id);
+                if (s._id != -1 && !s.removed) {
+                    cout << "Encontrado: " << s.name << " | Data: " << s.birth_date << endl;
+                } else {
+                    cout << "Estudante nao encontrado." << endl;
+                }
+                break;
+            }
+            case 4: {
+                cout << "Não implementado.";
+                break;
+            }
+            case 5: { // Excluir Estudante
+                cout << "ID para excluir: ";
+                cin >> id;
+                if (dao.remove(id)) cout << "Estudante excluido com sucesso." << endl;
+                else cout << "Erro ao excluir." << endl;
+                break;
+            }
+        }
+    } while (option != 0);
+
+    return 0;
+}
